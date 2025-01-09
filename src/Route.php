69d0a0d4cf538668e62c382fbce204428e37543d
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-01-09 13:41:18 +0800

    Set closure as global + update group params name

--- src/Route.php
+++ src/Route.php
@@ -2,6 +2,7 @@
 
 	namespace App\Routes;
 
+	use Closure;
 	use App\Routes\Scheme\Facade;
 	use App\Routes\Requests\Builder\{Delete, Get, Patch, Post, Put};
 	use App\Routes\Configurations\Builder\{Controller, Group, Middleware, Prefix, Name};
@@ -13,12 +14,12 @@
 	 * It allows defining routes for different HTTP methods (GET, POST, PUT, PATCH, DELETE) as well as
 	 * additional configuration options like controllers, middleware, and prefixes.
 	 *
-	 * @method static Put put(string $uri, string|array|\Closure $action = []) Defines a PUT route.
-	 * @method static Patch patch(string $uri, string|array|\Closure $action = []) Defines a PATCH route.
-	 * @method static Delete delete(string $uri, string|array|\Closure $action = []) Defines a DELETE route.
-	 * @method static Get get(string $uri, string|array|\Closure $action = []) Defines a GET route.
-	 * @method static Post post(string $uri, string|array|\Closure $action = []) Defines a POST route.
-	 * @method static Group group(array $routes, \Closure $action) Registers a group of routes with shared configurations and middleware, enhancing route organization and reusability.
+	 * @method static Put put(string $uri, string|array|Closure $action = []) Defines a PUT route.
+	 * @method static Patch patch(string $uri, string|array|Closure $action = []) Defines a PATCH route.
+	 * @method static Delete delete(string $uri, string|array|Closure $action = []) Defines a DELETE route.
+	 * @method static Get get(string $uri, string|array|Closure $action = []) Defines a GET route.
+	 * @method static Post post(string $uri, string|array|Closure $action = []) Defines a POST route.
+	 * @method static Group group(array $attributes, Closure $action) Registers a group of routes with shared configurations and middleware, enhancing route organization and reusability.
 	 * @method static Controller controller(string $className) Registers a controller.
 	 * @method static Middleware middleware(string|array $action) Registers middleware for the route.
 	 * @method static Prefix prefix(string $prefix) Adds a prefix to the route URI.
