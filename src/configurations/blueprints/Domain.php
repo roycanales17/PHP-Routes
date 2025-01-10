7bbde2710cc1be57f9c4e8876bb4543987b3ee79
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-01-10 19:42:51 +0800

    Setup domain route

--- src/configurations/blueprints/Domain.php
+++ src/configurations/blueprints/Domain.php
@@ -2,7 +2,39 @@
 
 	namespace App\Routes\Configurations\Blueprints;
 
+	use App\Routes\Scheme\Buffer;
+	use App\Routes\Scheme\Pal;
+
 	trait Domain
 	{
+		private string $domain = '';
 
+		protected function RegisterDomain(string $domain): void
+		{
+			$this->domain = strtolower($domain);
+		}
+
+		protected function DestroyDomain(): void
+		{
+			if ($this->getDomain()) {
+				$domain = Buffer::fetch('domain');
+				if ($domain) {
+					array_pop($domain);
+					Buffer::replace('domain', $domain);
+				}
+			}
+		}
+
+		protected function SetupDomain(): void
+		{
+			$domain = strtolower($this->getDomain());
+			if ($domain && in_array(strtolower(Pal::baseClassName(get_called_class())), Pal::getRoutes('configurations'))) {
+				Buffer::register('domain', $domain);
+			}
+		}
+
+		protected function getDomain(): string
+		{
+			return $this->domain;
+		}
 	}
--- src/configurations/builder/Domain.php
+++ src/configurations/builder/Domain.php
@@ -2,9 +2,39 @@
 
 	namespace App\Routes\Configurations\Builder;
 
+	use App\Routes\Configurations\Blueprints\Domain as BaseDomain;
+	use App\Routes\Configurations\Blueprints\Controller;
+	use App\Routes\Configurations\Blueprints\Group;
+	use App\Routes\Configurations\Blueprints\Middleware;
+	use App\Routes\Configurations\Blueprints\Name;
+	use App\Routes\Configurations\Blueprints\Prefix;
 	use App\Routes\Configurations\Config;
 
 	class Domain extends Config
 	{
+		private string $domainProperty;
 
+		use BaseDomain {
+			RegisterDomain as private domain;
+		}
+		use Controller {
+			RegisterController as public controller;
+		}
+		use Name {
+			RegisterName as public name;
+		}
+		use Group {
+			RegisterGroup as public group;
+		}
+		use Prefix {
+			RegisterPrefix as public prefix;
+		}
+		use Middleware {
+			RegisterMiddleware as public middleware;
+		}
+
+		function __construct(string $domain)
+		{
+			$this->domainProperty = $domain;
+		}
 	}
