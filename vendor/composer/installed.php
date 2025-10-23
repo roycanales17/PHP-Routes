590f10f50465b6001b885e551dbf18ded187f82b
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-10-23 19:50:02 +0800

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'roy404/routes',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ed4da8ebdd5cc854f6ead6ccfd23da08675b6d3d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'roy404/routes' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ed4da8ebdd5cc854f6ead6ccfd23da08675b6d3d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
